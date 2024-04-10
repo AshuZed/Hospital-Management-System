f1e64c9904fc2bfc723f8146d4effa35cd011112
nahom mitiku <106976757+nahilord124578@users.noreply.github.com>
2024-04-10 15:52:58 +0300

    Update App.js

--- client/src/pages/practitioner/doctor/App.js
+++ client/src/pages/practitioner/doctor/App.js
@@ -1,34 +1,12 @@
 import React from 'react';
 import './App.css';
 import './Navbar.css';
-import DoctorPage from './DoctorPage';
-import App1 from './App1';
-import {  Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
-import Sidebar from './Sidebar';
-import Medical from './Medical';
-import LabTestRequestPage from './LabTestRequestPage';
-import PrescriptionFormPage from './PrescriptionFormPage';
-import ReferralForm from './ReferralForm';
-
-
 function App() {
   return (
     <div>
     <Navbar/>
-    
-    <Routes>
-      <Route path='/' element={
-      <div><DoctorPage />
-      <Sidebar/>
-      </div>
-    }></Route>
-      <Route path="/App1" element={<App1/>}></Route>
-      <Route path="/Medical" element={<Medical/>}></Route>
-      <Route path="/LabTestRequestPage" element={<LabTestRequestPage/>}></Route>
-      <Route path="/PrescriptionFormPage" element={<PrescriptionFormPage/>}></Route>
-      <Route path="/ReferralForm" element={<ReferralForm/>}></Route>
-    </Routes>
+    <Routes/>
     </div>
   );
 }
